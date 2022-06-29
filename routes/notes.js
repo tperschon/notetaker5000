01902ffca71e050f370f82d1a03021de64c5f449
TPerschon <timperschon@gmail.com>
2022-06-29 17:20:29 -0600

    Refactored with read-write rather than require but seems unchanged

--- routes/notes.js
+++ routes/notes.js
@@ -10,14 +10,27 @@ notes.get('/notes', (req, res) => {
 notes.post('/notes', (req, res) => {
     if(req.body) {
         if(req.body.title && req.body.text) {
-            let notesJson = require('../db/notes.json');
-            let newNote = {
+            const newNote = {
                 title: req.body.title,
-                text: req.body.text,
+                body: req.body.text,
                 id: uuid()
             };
-            notesJson.push(newNote);
-            fs.writeFileSync(path.join(__dirname, '../db/notes.json'), JSON.stringify(notesJson, null, 4));
+            fs.readFile(path.join(__dirname, '../db/notes.json'), 'utf8', (err, data) => {
+              if (err) {console.error(err); }
+              else {
+                const allNotes = JSON.parse(data);
+                allNotes.push(newNote);
+                fs.writeFile(path.join(__dirname, '../db/notes.json'), JSON.stringify(allNotes, null, 4), err => { if(err) console.error(err) });
+              };
+            });
+            // let notesJson = require('../db/notes.json');
+            // let newNote = {
+            //     title: req.body.title,
+            //     text: req.body.text,
+            //     id: uuid()
+            // };
+            // notesJson.push(newNote);
+            // fs.writeFileSync(path.join(__dirname, '../db/notes.json'), JSON.stringify(notesJson, null, 4));
         }
         else {
             if(req.body.title) res.send('Body must have text.');
