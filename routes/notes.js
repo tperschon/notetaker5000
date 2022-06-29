a4d16dd5c775095bd073e0704767b5e9a7974e66
TPerschon <timperschon@gmail.com>
2022-06-29 15:56:12 -0600

    Notes routes start

---
+++ routes/notes.js
@@ -0,0 +1,15 @@
+const notes = require('express').Router();
+
+notes.get('/notes', (req, res) => {
+    
+});
+
+notes.post('/notes', (req, res) => {
+
+});
+
+notes.delete('/notes/:id', (req, res) => {
+
+});
+
+module.exports = notes;
