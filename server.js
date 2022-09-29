70ec4594e04a28669d5a70562105e5bc6d765484
TPerschon <timperschon@gmail.com>
2022-09-29 03:01:47 -0600

    Removed experimental traffic logging functionality

--- server.js
+++ server.js
@@ -2,20 +2,17 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index');
-const logTraffic = require('./middleware/logTraffic');
-const consolidateToJson = require('./utils/consolidateToJson');
 // set up app and port
 const app = express();
 const PORT = process.env.PORT || 3001;
 // set up imported routes to app
 app.use(express.json());
-app.use(express.urlencoded( { extended: true }))
-app.use(logTraffic);
+app.use(express.urlencoded( { extended: true }));
 app.use(express.static('public'));
 app.use('/', api);
 // notes PAGE route (not to be confused with GET/POST/etc. routes to api/notes)
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 // start app listening, notify console that app started
 app.listen(PORT, () => {
